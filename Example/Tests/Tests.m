6c8b457212521d12905b1525a27a1ab42784a909
Nathaniel Hamming <nhamming@ehealthinnovation.org>
2015-02-13 10:08:23 -0400

    Merged in task/13989-unit_tests
    Squashed commit of the following:
    
    commit f8bddafaa4cb5c9eaee71af0a5dc7f5deebf25d3
    Author: Nathaniel Hamming <nhamming@ehealthinnovation.org>
    Date:   Fri Feb 13 10:07:43 2015 -0400
    
        update to docs and tests
    
    commit 81c2f764901ad14c0b2ba507298c559ad2d397ff
    Author: Nathaniel Hamming <nhamming@ehealthinnovation.org>
    Date:   Wed Feb 11 15:04:27 2015 -0400
    
        building the demo app now depends on the tests
    
    commit db0d3229cecb2fc97851d8c441f080a146d6248b
    Author: Nathaniel Hamming <nhamming@ehealthinnovation.org>
    Date:   Wed Feb 11 15:02:56 2015 -0400
    
        Updated docs on test build
    
    commit d2e24f112218b424e93dcfd501b161b88c78abd7
    Author: Nathaniel Hamming <nhamming@ehealthinnovation.org>
    Date:   Wed Feb 11 15:02:48 2015 -0400
    
        Added unit tests
    
    commit aa00c5c3e1344a83b5699d73f6515e4800b2f085
    Author: Nathaniel Hamming <nhamming@ehealthinnovation.org>
    Date:   Wed Feb 11 15:01:43 2015 -0400
    
        Adding reference images to use for UI tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,37 +6,73 @@
 //  Copyright (c) 2014 Nathaniel Hamming. All rights reserved.
 //
 
-SpecBegin(InitialSpecs)
+#import <NHArrowView/NHArrowView.h>
 
-describe(@"these will fail", ^{
+SpecBegin(ArrowSpecs)
 
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
-
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
+describe(@"Arrow rotation", ^{
+    it(@"presents arrow view default angle", ^{
+        NHArrowView *view = [[NHArrowView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+        expect(view).to.haveValidSnapshot();
     });
     
-    it(@"will wait and fail", ^AsyncBlock {
-        
+    it(@"rotates arrow to 45 degrees", ^{
+        NHArrowView *view = [[NHArrowView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+        [view animatedRotateToRadian:DEGREES_TO_RADIANS(45.)];
+        expect([NSString stringWithFormat:@"%0.6f", view.transform.a]).to.equal(@"0.707107");
+        expect([NSString stringWithFormat:@"%0.6f", view.transform.b]).to.equal(@"0.707107");
+        expect([NSString stringWithFormat:@"%0.6f", view.transform.c]).to.equal(@"-0.707107");
+        expect([NSString stringWithFormat:@"%0.6f", view.transform.d]).to.equal(@"0.707107");
+        waitUntil(^(DoneCallback done) {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, (unsigned long) NULL), ^(void) {
+                [NSThread sleepForTimeInterval:1.0];
+                expect(view).to.to.haveValidSnapshotNamed(@"ArrowViewColourRedDegree45");
+                done();
+            });
+        });
+    });
+    
+    it(@"rotates arrow to 90 degrees", ^{
+        NHArrowView *view = [[NHArrowView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+        [view animatedRotateToDegree:90.];
+        // rounding a and d here, since they return a near-zero value
+        expect(round(view.transform.a)).to.equal(0);
+        expect(view.transform.b).to.equal(1);
+        expect(view.transform.c).to.equal(-1);
+        expect(round(view.transform.d)).to.equal(0);
     });
 });
 
-describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
+describe(@"Arrow style", ^{
+    it(@"arrow green colour", ^{
+        NHArrowView *view = [[NHArrowView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+        view.strokeColor = [UIColor greenColor];
+        view.fillColor = [UIColor greenColor];
+        expect(view).to.haveValidSnapshot();
     });
     
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
+    it(@"arrow with long head", ^{
+        NHArrowView *view = [[NHArrowView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+        view.headLength = 50.;
+        expect(view).to.haveValidSnapshot();
     });
     
-    it(@"will wait and succeed", ^AsyncBlock {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            done();
-        });
+    it(@"arrow with wide head", ^{
+        NHArrowView *view = [[NHArrowView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+        view.headWidth = 50.;
+        expect(view).to.haveValidSnapshot();
+    });
+    
+    it(@"arrow with wide tail", ^{
+        NHArrowView *view = [[NHArrowView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+        view.tailWidth = 50.;
+        expect(view).to.haveValidSnapshot();
+    });
+    
+    it(@"arrow with thick stroke", ^{
+        NHArrowView *view = [[NHArrowView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+        view.strokeWidth = 10.;
+        expect(view).to.haveValidSnapshot();
     });
 });
 
